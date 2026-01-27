8582a0c072fa7fb54624ac775bbd5748cf96e380
Subham Sharma <subham.sharma221296@gmail.com>
2026-01-27 13:05:35 +0530

    Firebase connection success

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {getDatabase, ref, set} from 'firebase/database'
+import {app} from './Firebase'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const db = getDatabase(app);
 
+function App() {
+  const put = () =>{
+    set(ref(db,'users/01'),{
+      id: 1,
+      name: "Subham",
+      age: 28
+    });
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={put}>Put Data </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
