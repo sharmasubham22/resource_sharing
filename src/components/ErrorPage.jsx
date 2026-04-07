0a1e6ca2616e0355b7a4772402a7e85e397ec0f6
Subham Sharma <subham.sharma221296@gmail.com>
2026-04-07 16:17:14 +0530

    404 page added

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { logoVector } from '../assets/Elements'
+
+export default function ErrorPage() {
+  return (
+    <div>
+        <a
+            href="/"
+            className="flex items-center p-3 font-body"
+          >
+            {logoVector}
+            <span className="self-center text-xl text-text-primary font-bold whitespace-nowrap">
+              StackXchange
+            </span>
+          </a>
+        <div className='min-h-screen flex items-center justify-center'>
+      <h1 className='text-3xl text-text-primary font-heading'>404 - Page Not Found</h1>
+      </div>
+    </div>
+  )
+}
